31ba8e1605a7f1d6c77d574d6dad08c4b9463c7b
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-02-27 15:45:51 +0100

    Added DisplayName to Employee model

--- Business/Models/Employee.cs
+++ Business/Models/Employee.cs
@@ -7,6 +7,7 @@ public class Employee
     public int Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
+    public string DisplayName => $"{FirstName} {LastName}";
 
     public Role? Role { get; set; } = null!;
 }
